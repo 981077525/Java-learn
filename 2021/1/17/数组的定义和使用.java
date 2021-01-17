89e210c4902d2f32b5a487b9bc42a6fa048b2815
981077525 <981077525@qq.com>
2021-01-18 00:00:52 +0800

    2021\1\17\数组的定义和使用.java

--- 2021/1/17/数组的定义和使用.java
+++ 2021/1/17/数组的定义和使用.java
@@ -6,21 +6,21 @@
 
  */
 
-public class arrary{
+public class Array{
     public static void main(String[] args)
     {
         //定义一个元素个数为5的整型数组，不进行赋值初始化
         int[] arr = new int[5];
 
         //定义一个元素个数为4的整型数组，进行初始化
-        int[] arr1 = new int[4] {0 , 1 , 2 , 3};
+        int[] arr1 = new int[]{0, 1, 2, 3};
         //遍历的第一种方法：
         for(int i =0;i<arr1.length;i++)
         {
             System.out.println(arr1[i]);
         }
         //遍历的第二种方法：
-        for(int b;arr1)
+        for(int b: arr1)
         {
             System.out.println(b);
         }
