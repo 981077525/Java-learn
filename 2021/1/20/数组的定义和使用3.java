726e77b417089aa0d04d48bb08e42d9dd3859640
981077525 <981077525@qq.com>
2021-01-22 00:01:59 +0800

    2021\1\20\数组的定义和使用3.java

--- 2021/1/20/数组的定义和使用3.java
+++ 2021/1/20/数组的定义和使用3.java
@@ -4,5 +4,13 @@ public class Two_Three {
         String[] arr = new String[5];
         //再赋值
         arr = new String[]{"Hello", "World", "CSDN", "QiuFengYaJu"};
+
+        for(int i=0;i<arr.length;i++){
+            System.out.printf("%s " ,arr[i]);
+        }
+        System.out.printf("\n------------------\n");
+        for(String b:arr) {
+            System.out.printf("%s " ,b);
+        }
     }
 }
