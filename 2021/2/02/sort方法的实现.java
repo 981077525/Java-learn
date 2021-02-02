4067bd711f147c6a0c2f2028a554d3cfc1e7168b
981077525 <981077525@qq.com>
2021-02-02 11:04:12 +0800

    sort方法的实现

---
+++ 2021/2/02/sort方法的实现.java
@@ -0,0 +1,16 @@
+    /*
+    *   sort方法的实现
+    *   这里仅对int类型的数组进行实现
+    * */
+    public static void sort(int[] arr){
+        int max = arr[0];
+        for(int i = 0;i<arr.length;i++)
+        {
+            for(int j = i + 1;j<arr.length;j++)
+            if(arr[i] > arr[j]){
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    };
