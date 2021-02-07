6beb9bbd9864df8cf2fda65be881db4efbadfb0c
981077525 <981077525@qq.com>
2021-02-07 23:07:12 +0800

    打印二维数组

--- 2021/2/07/打印二维数组.java
+++ 2021/2/07/打印二维数组.java
@@ -5,12 +5,14 @@ public class 打印二维数组 {
         int[][] arr = {
                 {1,2,3,4},
                 {5,6,7,8},
+                null,
                 {9,10,11,12},
                 {13,14,15,16},
                 {17,18,19,20}
         };
-        printArrays(null);
-        System.out.println(Arrays.deepToString(null));
+//        System.out.println(arr.length);
+        printArrays(arr);
+        System.out.println(Arrays.deepToString(arr));
     }
 
     /*
@@ -23,12 +25,16 @@ public class 打印二维数组 {
         }else{
             System.out.println("=====================");
             for(int i = 0;i<arr.length;i++){
-                String ans = "[";
-                for(int j = 0;j<arr[i].length - 1;j++){
-                    ans += arr[i][j] + ", ";
+                if(arr[i] == null){
+                    System.out.println("null");
+                }else{
+                    String ans = "[";
+                    for(int j = 0;j<arr[i].length - 1;j++){
+                        ans += arr[i][j] + ", ";
+                    }
+                    ans += arr[i][arr[i].length - 1] +"]";
+                    System.out.println(ans);
                 }
-                ans += arr[i][arr[i].length - 1] +"]";
-                System.out.println(ans);
             }
             System.out.println("=====================");
         }
