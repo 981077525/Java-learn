99186a784156b7bc6083f4994bad5c302ebd0011
981077525 <981077525@qq.com>
2021-02-08 23:16:49 +0800

    自定义类的更新：增加了自定义的方法

--- 2021/2/08/Student.java
+++ 2021/2/08/Student.java
@@ -3,4 +3,8 @@ public class Student {
     String name;        // 姓名
     int age;            //年龄
     int height;         // 身高
+
+    public void myselfInformence(){
+        System.out.println("大家好，我叫" + name + ",我今年" + age + "岁了，我身高" + height);
+    }
 }
