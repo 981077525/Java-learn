2c59f4f43dd9d7f9b026d6022794596e03f65544
981077525 <981077525@qq.com>
2021-02-10 13:08:31 +0800

    this关键字的两种使用： 1. 作为调用对象的引用使用 2. 作为当前类实例化后的对象的引用使用

--- 2021/2/10/Teacher.java
+++ 2021/2/10/Teacher.java
@@ -9,17 +9,18 @@ public class Teacher {
 
     // 无参数列表的构造器
     public Teacher(){
-
+        this(null,0,null);
     }
 
     // 含有 name 的参数构造器
     public Teacher(String name){
+        this(name,0,null);
 
     }
 
     // 含有  name 和 age 的参数列表的构造器
     public Teacher(String name,int age){
-
+        this(name,age,null);
     }
 
     // 含有 name 和 age 和 courseArrays
