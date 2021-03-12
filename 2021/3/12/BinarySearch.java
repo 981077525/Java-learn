ffd57302d25a3f823db3ac4a92dcfdd703803047
981077525 <981077525@qq.com>
2021-03-12 23:26:42 +0800

    使用面向对象的思想来实现二分查找binarysearch

---
+++ 2021/3/12/BinarySearch.java
@@ -0,0 +1,32 @@
+/*
+*   通过面向对象的思想来实现二分查找
+* */
+class Range{
+    private long[] arr;
+    private int lowIndex;
+    private int hightIndex;
+
+
+    // 构造器,构建对象
+    public Range(long[] arr){
+        this.arr = arr;
+        lowIndex = 0;
+        hightIndex = arr.length - 1;
+    }
+
+    // 获取
+    public
+}
+public class BinarySearch {
+
+    // 定义的属性，用来保存获取到的数组信息
+    public long[] arr;
+
+    // 构造器，获取传入的参数
+    public BinarySearch(long[] arr){
+        this.arr = arr;
+    }
+
+    Range range = new Range(arr);
+
+}
