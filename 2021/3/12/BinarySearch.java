4a043ed7ec1699289f437f5455b98ad805c17fae
981077525 <981077525@qq.com>
2021-03-14 20:24:03 +0800

    使用面向对象的思想实现二分查找

--- 2021/3/12/BinarySearch.java
+++ 2021/3/12/BinarySearch.java
@@ -1,32 +1,62 @@
-/*
-*   通过面向对象的思想来实现二分查找
-* */
 class Range{
-    private long[] arr;
+    private int[] arr;
+
     private int lowIndex;
     private int hightIndex;
 
-
-    // 构造器,构建对象
-    public Range(long[] arr){
+    private int middleIndex;
+    private int middleValue;
+    public Range(int[] arr){
         this.arr = arr;
-        lowIndex = 0;
-        hightIndex = arr.length - 1;
+        this.lowIndex = 0;
+        this.hightIndex = arr.length - 1;
+        this.middleIndex = (hightIndex + lowIndex) / 2;
+        this.middleValue = arr[middleIndex];
     }
 
-    // 获取
-    public
+    // 获取对象长度或者元素的个数
+    public int getSize(){
+        return hightIndex - lowIndex;
+    }
+    // 获取元素中间元素的下标
+    public int getMiddleIndex(){
+        return middleIndex;
+    }
+    // 获取对象中间元素的值
+    public int getMiddleValue(){
+        return middleValue;
+    }
+    // 丢弃左半部分的数组
+    public void discardLeftPart(){
+        lowIndex = middleIndex;
+        middleIndex = ((hightIndex + lowIndex + 1) / 2);
+        middleValue = arr[middleIndex];
+    }
+    // 丢弃右半部分的数组
+    public void discardRightPart(){
+        hightIndex = middleIndex;
+        middleIndex = ((hightIndex + lowIndex - 1) / 2) ;
+        middleValue = arr[middleIndex];
+    }
 }
-public class BinarySearch {
+/*
+*   使用面向对象的思想实现二分查找
+* */
+public class BinarySearch{
+    public static int binarySearch(int[] arr, int target) {
+        // 实例化这个对象
+        Range range = new Range(arr);
 
-    // 定义的属性，用来保存获取到的数组信息
-    public long[] arr;
-
-    // 构造器，获取传入的参数
-    public BinarySearch(long[] arr){
-        this.arr = arr;
+        while(range.getSize() > 0 ){
+            if(range.getMiddleValue() == target){
+                return range.getMiddleIndex();
+            }else if(range.getMiddleValue() < target){
+                range.discardLeftPart();
+            }else{
+                range.discardRightPart();
+            }
+        }
+        // 返回 -1 即为没有找到
+        return -1;
     }
-
-    Range range = new Range(arr);
-
 }
