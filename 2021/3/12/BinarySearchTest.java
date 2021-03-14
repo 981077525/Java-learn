2a677b34155f9b88854d38ca1fdb9c132b4fe563
981077525 <981077525@qq.com>
2021-03-14 20:24:38 +0800

    使用面向对象的思想实现二分查找

---
+++ 2021/3/12/BinarySearchTest.java
@@ -0,0 +1,18 @@
+public class BinarySearchTest{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        System.out.println(BinarySearch.binarySearch(arr, 1));
+        System.out.println(BinarySearch.binarySearch(arr, 2));
+        System.out.println(BinarySearch.binarySearch(arr, 3));
+        System.out.println(BinarySearch.binarySearch(arr, 4));
+        System.out.println(BinarySearch.binarySearch(arr, 5));
+        System.out.println(BinarySearch.binarySearch(arr, 6));
+        System.out.println(BinarySearch.binarySearch(arr, 7));
+        System.out.println(BinarySearch.binarySearch(arr, 8));
+        System.out.println(BinarySearch.binarySearch(arr, 9));
+        System.out.println(BinarySearch.binarySearch(arr, 10));
+        System.out.println(BinarySearch.binarySearch(arr, 11));
+
+    }
+}
