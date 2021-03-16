06073a11094278cd64728ae30fa0ebadde21c9af
981077525 <981077525@qq.com>
2021-03-16 23:29:39 +0800

    类的继承

---
+++ 2021/3/16/ClassExtend/ClassExtend.java
@@ -0,0 +1,14 @@
+package ClassExtend;
+
+public class ClassExtend {
+    public static void main(String[] args) {
+        Phone huawei = new Phone("huawei");
+        huawei.hight = 10.00;
+        huawei.length = 50.00;
+        huawei.width = 50.00;
+        huawei.power = 4000;
+
+        Ipad iphone = new Ipad("ipad");
+
+    }
+}
