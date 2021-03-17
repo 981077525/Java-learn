3818db02db0dfee8a4d1505c3784612ae18935e2
小鹏 <74500183+981077525@users.noreply.github.com>
2021-03-17 22:34:27 +0800

    Add files via upload

---
+++ 2021/3/MyListNode.java
@@ -0,0 +1,12 @@
+// 双向链表的结点类
+// 元素类型不再使用 int 这种基本类型
+// 而是使用 String 这种引用类型
+public class MyListNode {
+    public String val;
+    public MyListNode prev; // 指向前驱结点；头结点的 prev = null
+    public MyListNode next; // 指向后继结点；尾节点的 next = null
+
+    public MyListNode(String val) {
+        this.val = val;
+    }
+}
